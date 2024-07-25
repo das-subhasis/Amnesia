b973b6735f0c21861b7d84ba71e69db15b95370d
Subhasis Das <das.shubasis2003@gmail.com>
2024-07-25 23:25:23 +0530

    UPDATE index.ts

--- BackEnd/src/index.ts
+++ BackEnd/src/index.ts
@@ -24,7 +24,7 @@ app.use(notFoundHandler)
 app.use(errorHandler)
 
 app.get('/', (req, res) => res.send("Hi"))
-app.listen(PORT, () => {
+app.listen(3000, () => {
     console.log(`Server is running on port ${PORT}`);
 })
 
