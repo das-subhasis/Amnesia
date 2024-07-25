718d68a4d3ba0ed7b685006448803540dc64622a
Subhasis Das <das.shubasis2003@gmail.com>
2024-07-25 23:30:06 +0530

    UPDATE index.js

--- BackEnd/api/index.js
+++ BackEnd/api/index.js
@@ -1,3 +1,31 @@
-import app from '../src/index'
+import express from "express";
+import { PORT } from "../dist/utils/utils";
+import CORS from "cors"
+import { errorHandler, notFoundHandler } from "../dist/middlewares/errorMiddleware";
+import product from "../dist/routes/productRoutes";
+import order from "../dist/routes/orderRoutes";
+import connectDB from "../dist/utils/config";
+import auth from "../dist/routes/authRoutes";
+import user from "../dist/routes/userRoutes";
+ 
+connectDB();
+
+const app = express();
+
+app.use(express.json())
+app.use(CORS())
+
+app.use('/api/auth', auth);
+app.use('/api/products', product);
+app.use('/api/orders', order);
+app.use('/api/user', user);
+
+app.use(notFoundHandler)
+app.use(errorHandler)
+
+app.get('/', (req, res) => res.send("Hi"))
+app.listen(3000, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
 
 module.exports = app;
