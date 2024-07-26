79ab23d3dec635c5b306b317c3aa99b61bf91b32
Subhasis Das <das.shubasis2003@gmail.com>
2024-07-26 11:35:37 +0530

    UPDATE index.js

--- BackEnd/api/index.js
+++ BackEnd/api/index.js
@@ -7,7 +7,7 @@ import order from "../src/routes/orderRoutes";
 import connectDB from "../src/utils/config";
 import auth from "../src/routes/authRoutes";
 import user from "../src/routes/userRoutes";
- 
+
 connectDB();
 
 const app = express();
@@ -19,11 +19,11 @@ app.use('/api/auth', auth);
 app.use('/api/products', product);
 app.use('/api/orders', order);
 app.use('/api/user', user);
-
+app.use('/', (req, res) => res.json({ message: "Welcome to my api"}))
 app.use(notFoundHandler)
 app.use(errorHandler)
 
-app.get('/', (req, res) => res.send("Hi"))
+
 app.listen(3000, () => {
     console.log(`Server is running on port ${PORT}`);
 })
