e2dd7275e9febd72b20c00efac69428668a93bd8
Subhasis Das <das.shubasis2003@gmail.com>
2024-07-29 22:48:43 +0530

    UPDATE route

--- BackEnd/src/routes/productRoutes.ts
+++ BackEnd/src/routes/productRoutes.ts
@@ -10,8 +10,6 @@ product.get('/id/:id', getProductById);
 
 product.get('/:product_name', getProducts);
 
-product.get('/c/:id', getProductByCategory);
-
 product.get('/c/:category', getProductByCategory);
 
 product.post('/add', authenticate, addProduct);
